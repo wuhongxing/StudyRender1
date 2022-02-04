5471406641624a7fe971267b23bdbe2515db1347
吴红星 <wuhongxing0512@zto.com>
2022-02-04 10:59:12 +0800

    Initial Commit

---
+++ StudyRender1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudyRender1
+//
+//  Created by 吴红星 on 2022/2/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StudyRender1UITests/StudyRender1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  StudyRender1UITestsLaunchTests.swift
+//  StudyRender1UITests
+//
+//  Created by 吴红星 on 2022/2/4.
+//
+
+import XCTest
+
+class StudyRender1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
